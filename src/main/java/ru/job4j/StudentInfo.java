a6662769c460f227a94a78f349feee2ed9bb2779
svoh86 <sms-86@mail.ru>
2022-01-16 13:31:14 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Mikhail Svistunov");
+        System.out.println("08.04.1986");
+    }
+}
