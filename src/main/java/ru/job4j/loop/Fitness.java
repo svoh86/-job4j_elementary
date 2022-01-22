3a635c7c384dc42433ccf5e93dc218f2012f0e1e
svoh86 <sms-86@mail.ru>
2022-01-22 18:10:10 +0300

    Протеиновая диета [#156313]

---
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan <= nik) {
+            month += 1;
+            ivan *= 3;
+            nik *= 2;
+        }
+        return month;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(10, 50));
+    }
+}
