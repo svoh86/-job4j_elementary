f1f6ecf64a5406208298561972424a623cc395fc
svoh86 <sms-86@mail.ru>
2022-01-26 15:47:53 +0300

    Слово заканчивается на ... [#156317 #255902]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -2,12 +2,11 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = true;
         for (int i = 0; i < post.length; i++) {
             if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
                 return false;
             }
         }
-        return result;
+        return true;
     }
 }
