ee589d88ca5aaf3e178047d41b36702deab5cac2
svoh86 <sms-86@mail.ru>
2022-01-26 13:31:35 +0300

    Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -24,4 +24,24 @@ public class FindLoopTest {
         int rsl = FindLoop.indexOf(data, el);
         Assert.assertEquals(expected, rsl);
     }
+
+    @Test
+    public void whenFind7() {
+        int[] data = new int[] {5, 7, 3};
+        int el = 7;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 3;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
 }
