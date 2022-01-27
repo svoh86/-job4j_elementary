97127f80ba1113b060e45b49876c3008fd9d6239
svoh86 <sms-86@mail.ru>
2022-01-27 16:10:42 +0300

    Дефрагментация массива [#225]

---
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                for (int i = index + 1; i < array.length; i++) {
+                    if (array[i] != null) {
+                        String temp = array[index];
+                        array[index] = array[i];
+                        array[i] = temp;
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+}
