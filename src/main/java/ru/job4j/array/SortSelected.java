fbc7c407a04ed501baf05438e14aca7977ac7f43
svoh86 <sms-86@mail.ru>
2022-07-12 14:25:12 +0300

    Сортировка выборкой

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -1,6 +1,14 @@
 package ru.job4j.array;
 
 public class SortSelected {
+    /**
+     * На первом этапе мы ищем минимальный элемент во всем массиве.
+     * Далее ищем его индекс и меняем местами с нулевым элементом.
+     * Повторяем процедуру поиска минимального элемента, но ищем уже с элемента с индексом 1.
+     *
+     * @param data массив
+     * @return массив отсортированный выборкой
+     */
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
